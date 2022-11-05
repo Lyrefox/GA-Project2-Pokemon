8cc65386ff986951435866d31be0ed7efed2f750
Matthew Lea <matthewlea8@gmail.com>
2022-11-05 21:07:29 +1100

    pictures, pokedex number working. Filter via generation working. Search bar not working, sql not accepting (i think it is due to how python is formatting a string for an html input as sql text need to be in ' ' quotes

--- server.py
+++ server.py
@@ -16,21 +16,31 @@ def main():
 def main_gen():
     select = request.form.get('gen_choice')
     print(select)
+    name_search = request.form.get('pokesearch')
+    print(name_search)
+    # print(name)
     pokemon = []
-    if select == 'all':
-        db_data = sql_execute('SELECT name, generation, image, pokedex FROM pokemon ORDER BY pokedex ASC')
-        pokemon = []
-        for dbdata in db_data:
-            name, generation, image, pokedex = dbdata
-            pokemon.append([name, generation, image, pokedex])
-        return render_template('index.html', select=select, pokemon=pokemon)
+    if name_search == "":
+        if select == 'all':
+            db_data = sql_execute('SELECT name, generation, image, pokedex FROM pokemon ORDER BY pokedex ASC')
+            pokemon = []
+            for dbdata in db_data:
+                name, generation, image, pokedex = dbdata
+                pokemon.append([name, generation, image, pokedex])
+            return render_template('index.html', select=select, pokemon=pokemon)
+        else:
+            db_data = sql_execute('SELECT name, generation, image, pokedex FROM pokemon WHERE generation = %s ORDER BY pokedex ASC', [select])
+            for dbdata in db_data:
+                name, generation, image, pokedex = dbdata
+                pokemon.append([name, generation, image, pokedex])
+            return render_template('index.html', select=select, pokemon=pokemon)
     else:
-        db_data = sql_execute('SELECT name, generation, image, pokedex FROM pokemon WHERE generation = %s ORDER BY pokedex ASC', [select])
+        db_data = sql_execute(f'SELECT name, generation, image, pokedex FROM pokemon WHERE name = %s ORDER BY pokedex ASC', [name_search])
+        print(db_data)
         for dbdata in db_data:
             name, generation, image, pokedex = dbdata
             pokemon.append([name, generation, image, pokedex])
         return render_template('index.html', select=select, pokemon=pokemon)
- 
 
 @app.route('/login')
 def login_form():
